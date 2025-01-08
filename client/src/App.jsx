cb2ea7af7c3429929812aebdabb185849ae36a65
SiyathmaWedamulla <wedamullasiyathma@gmail.com>
2025-01-08 08:52:35 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Home from './pages/home/Home';
+
+function App() {
+
+
+  return (
+  <Router>
+    <Routes>
+    <Route path="/" element={<Home/>} />
+    </Routes>
+  </Router>
+      
+    
+  )
+}
+
+export default App
